e1846f9c96d727b068ac65ed8bb12f5441b4d346
ya_pan <80136652+nomoreexpect@users.noreply.github.com>
2024-05-21 19:39:20 +0800

    Create isPalindrome_125.java

---
+++ Algorithm/Point/src/isPalindrome_125.java
@@ -0,0 +1,28 @@
+/**
+ * @Author:PanYa
+ * @Date 2024/5/21-下午7:33
+ * @Description:
+ */
+public class isPalindrome_125 {
+    public boolean isPalindrome(String s) {
+        // 转换为小写并移除所有非字母数字字符
+        StringBuilder filtered = new StringBuilder();
+        for (char c : s.toCharArray()) {
+            if (Character.isLetterOrDigit(c)) {
+                filtered.append(Character.toLowerCase(c));
+            }
+        }
+
+        // 使用双指针法检查回文
+        int left = 0, right = filtered.length() - 1;
+        while (left < right) {
+            if (filtered.charAt(left) != filtered.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+
+        return true;
+    }
+}
