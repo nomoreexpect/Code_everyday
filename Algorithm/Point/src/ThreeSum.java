f05cf3903c8fbfd729ab25443bd1326db8ae421a
ya_pan <80136652+nomoreexpect@users.noreply.github.com>
2024-05-21 15:40:19 +0800

    Update README.md

---
+++ Algorithm/Point/src/ThreeSum.java
@@ -0,0 +1,38 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * @Author:PanYa
+ * @Date 2024/5/21-下午3:32
+ * @Description:
+ */
+public class ThreeSum {
+    public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length ; i++) {
+            if (nums[i] > 0) {return res;}
+            if (i > 0 && nums[i] == nums[i-1]) {continue;}
+            int left = i+1, right = nums.length - 1;
+            while (left < right) {
+                int sum = nums[i] + nums[left] + nums[right];
+                if(sum>0){right--;} else if (sum < 0) {
+                    left++;
+                }else {
+                    res.add(Arrays.asList(nums[i], nums[left], nums[right]));
+                    while (left < right && nums[right] == nums[right - 1]) {
+                        right--;
+                    }
+                    while (left < right && nums[left] == nums[left + 1]) {
+                        left++;
+                    }
+                    right--;
+                    left++;
+                }
+            }
+
+        }
+        return res;
+    }
+}
